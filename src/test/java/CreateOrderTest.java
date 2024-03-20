6c97cfd5c134ab44b6b4c44babee02b3b7f3a17e
NastyaNikiforova <danilova-anastaska@yandex.ru>
2024-03-20 14:19:31 +0300

    внесены изменения в CreateOrderTest.java

--- src/test/java/CreateOrderTest.java
+++ src/test/java/CreateOrderTest.java
@@ -1,8 +1,8 @@
-import com.google.gson.Gson;
 import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.example.Order;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,36 +13,20 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 
 @RunWith(Parameterized.class)
 public class CreateOrderTest {
-    Gson gson = new Gson();
-    private String firstName;
-    private String lastName;
-    private String address;
-    private int metroStation;
-    private String phone;
-    private int rentTime;
-    private String deliveryDate;
-    private String comment;
-    private String[] color;
 
-    public CreateOrderTest(String firstName, String lastName, String address, int metroStation, String phone, int rentTime, String deliveryDate, String comment, String[] color) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.metroStation = metroStation;
-        this.phone = phone;
-        this.rentTime = rentTime;
-        this.deliveryDate = deliveryDate;
-        this.comment = comment;
-        this.color = color;
+    private Order order;
+
+    public CreateOrderTest(Order order) {
+        this.order = order;
     }
 
     @Parameterized.Parameters
     public static Object[] getOrderData() {
         return new Object[][] {
-                {"Иван", "Синичка", "Москва, Луговая, 4", 4, "+79008005566", 3, "2024-04-15", "Перезвоните", new String[]{"BLACK"}},
-                {"Мария", "Облако", "Москва, Волгоградский проспект", 15, "89003005050", 2, "2024-07-23", "", new String[]{"GREY"}},
-                {"Виктор", "Комод", "Москва, Козицкий переулок, 2", 48, "+78005007000", 5, "2024-03-25", "", new String[]{"BLACK", "GREY"}},
-                {"Пётр", "Иванов", "Москва, Волгоградский проспект", 15, "89003005050", 2, "2024-07-23", "", new String[]{}}
+                {new Order("Иван", "Синичка", "Москва, Луговая, 4", 4, "+79008005566", 3, "2024-04-15", "Перезвоните", new String[]{"BLACK"})},
+                {new Order("Мария", "Облако", "Москва, Волгоградский проспект", 15, "89003005050", 2, "2024-07-23", "", new String[]{"GREY"})},
+                {new Order("Виктор", "Комод", "Москва, Козицкий переулок, 2", 48, "+78005007000", 5, "2024-03-25", "", new String[]{"BLACK", "GREY"})},
+                {new Order("Пётр", "Иванов", "Москва, Волгоградский проспект", 15, "89003005050", 2, "2024-07-23", "", new String[]{})}
         };
     }
     @Before
@@ -51,10 +35,10 @@ public class CreateOrderTest {
     }
 
     @Step("Send POST request to /api/v1/orders to create new order")
-    public Response sendPostRequestToCreateNewOrder(String json) {
+    public Response sendPostRequestToCreateNewOrder() {
         Response response = given()
                 .header("Content-type", "application/json")
-                .body(json)
+                .body(order)
                 .when()
                 .post("/api/v1/orders");
         return response;
@@ -68,7 +52,7 @@ public class CreateOrderTest {
     @Test
     @DisplayName("Create new order and check status code")
     public void createNewOrderAndCheck201StatusCode() {
-        Response response = sendPostRequestToCreateNewOrder(gson.toJson(getOrderData()));
+        Response response = sendPostRequestToCreateNewOrder();
         check201StatusCodeAndCompareResponseBody(response);
         int orderTrack = response.then().extract().path("track");
         System.out.println(orderTrack);
